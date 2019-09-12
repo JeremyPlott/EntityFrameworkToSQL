0393b4e61199b0972237c417a1a6cb66c8d5018f
Jeremy <JeremyPlott@gmail.com>
2019-09-12 09:33:26 -0400

    Added vendor repository

--- EntityFrameworkToSQL/Program.cs
+++ EntityFrameworkToSQL/Program.cs
@@ -9,6 +9,53 @@ namespace EntityFrameworkToSQL {
 
             using (var context = new PRSdbContext()) { // using statement isn't necessary, but it will automatically dispose of this once it's finished. Efficiency.
 
+                //var request = new Requests() {
+                //    Id = 0,
+                //    Description = "Another new request",
+                //    Justification = "Moreee data in the table",
+                //    RejectionReason = null,
+                //    DeliveryMode = "Pickup",
+                //    Status = "NEW",
+                //    Total = 0,
+                //    UserId = context.Users.SingleOrDefault(u => u.Username.Equals("NotAGiraffe")).Id
+                //};
+                //context.Requests.Add(request);
+
+                //var request = new Requests() { Id = 3, Description = "Changed again" };
+                //var dbRequest = context.Requests.Find(request.Id);
+                //dbRequest.Description = request.Description;
+
+                ////dbRequest = context.Requests.Find(3);
+                ////context.Requests.Remove(dbRequest);
+                //context.SaveChanges();
+
+                //request, status, total, then line items for name qty price
+
+
+                var req3 = context.Requests.Find(3);
+
+                req3.Total = req3.RequestLines.Sum(l => l.Product.Price * l.Quantity);
+                context.SaveChanges();
+
+                Console.WriteLine($"{req3.Description}, {req3.Status}, {req3.Total.ToString("C")}");
+                req3.RequestLines.ToList().ForEach(rl => {
+                    Console.WriteLine($"{rl.Product.Name,-10} {rl.Quantity,5} " +
+                        $"{rl.Product.Price.ToString("C"),10} " +
+                        $"{(rl.Product.Price * rl.Quantity).ToString("C"),11}");
+                });
+                Console.WriteLine("");
+
+                var allreq = (from r in context.Requests.ToList()
+                              select r.Total).Sum();
+
+                //var total = context.Requests.Sum(r => r.Total); // alternative with lambda syntax for above line
+
+                Console.WriteLine("~~~~");
+                Console.WriteLine($"Sum of request cost: {allreq.ToString("C")}");
+                Console.WriteLine("~~~~");
+
+
+
                 var vendors = context.Vendors.ToList(); // run a quick-watch on this and it shows assoc products as well
 
                 vendors.ForEach(v => Console.WriteLine($"{v.Code} {v.Name}"));
@@ -21,7 +68,10 @@ namespace EntityFrameworkToSQL {
 
 
 
-                var users = context.Users.ToList();
+                //var users = context.Users.ToList();
+                var users = from u in context.Users.ToList()
+                            where u.Username.Contains("r") || u.Username.Contains("n")
+                            select u;
 
                 foreach(var user in users) {
                     Console.WriteLine($"{user.Username} is {user.Firstname} {user.Lastname}");
---
+++ EntityFrameworkToSQLLibrary/VendorRepository.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using EntityFrameworkToSQL;
+using System.Linq;
+
+namespace EntityFrameworkToSQLLibrary {
+
+    class VendorRepository {
+
+        private static PRSdbContext context = new PRSdbContext();
+
+        public static List<Vendors> GetAll() {
+            return context.Vendors.ToList();
+        }
+        public static Vendors GetByPK(int id) {
+            if (id < 1) { throw new Exception("Vendor Id must be greater than 0"); }
+            return context.Vendors.Find(id);
+        }
+        public static bool Insert(Vendors vendor) {
+            if (vendor == null) { throw new Exception("Vendor instance must not be null"); }
+            vendor.Id = 0;
+            context.Vendors.Add(vendor);
+            return context.SaveChanges() == 1;
+        }
+        public static bool Update(Vendors vendor) {
+            if (vendor == null) { throw new Exception("Vendor not found"); }
+            var dbvendor = context.Vendors.Find(vendor.Id);
+            if (dbvendor == null) { throw new Exception("Vendor not found"); }
+            dbvendor.Id = vendor.Id;
+            dbvendor.Name = vendor.Name;
+            dbvendor.Code = vendor.Code;
+            dbvendor.Products = vendor.Products;
+            dbvendor.Address = vendor.Address;
+            dbvendor.Zip = vendor.Zip;
+            dbvendor.City = vendor.City;
+            dbvendor.State = vendor.State;
+            dbvendor.Email = vendor.Email;
+            return context.SaveChanges() == 1;
+        }
+        public static bool Delete(Vendors vendor) {
+            if (vendor == null) { throw new Exception("Vendor not found"); }
+            var dbvendor = context.Vendors.Find(vendor.Id);
+            if (dbvendor == null) { throw new Exception("Vendor not found"); }
+            context.Vendors.Remove(dbvendor);
+            return context.SaveChanges() == 1;
+        }
+        public static bool Insert(int id) {
+            var vendor = context.Vendors.Find(id);
+            if (vendor == null) { return false; }
+            var rc = Delete(vendor);
+            return rc;
+        }
+    }
+}
